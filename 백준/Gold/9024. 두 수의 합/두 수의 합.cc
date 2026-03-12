1007162538cd61f1fb2598c0d4945804c77f8bd5
KangwWook Seo <103990401+kangwookseo@users.noreply.github.com>
2026-03-12 21:35:38 +0900

    [Gold V] Title: 두 수의 합, Time: 408 ms, Memory: 9952 KB -BaekjoonHub

---
+++ 백준/Gold/9024. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int t;
+	std::cin >> t;
+
+	while (t-- > 0)
+	{
+		int n;
+		long long k;
+		std::cin >>n>>k;
+
+		std::vector<long long> cont(n);
+		
+		for (int idx = 0; idx < n; ++idx)
+		{
+			std::cin >> cont[idx];
+		}
+		std::sort(cont.begin(), cont.end());
+
+		int l_p = 0;
+		int r_p = n - 1;
+
+		long long min_diff = 200000011;
+		int count = 0;
+
+		while(l_p <r_p)
+		{
+			long long sum = cont[l_p] + cont[r_p];
+			long long diff = std::abs(sum - k);
+
+			if (diff<min_diff)
+			{
+				min_diff = diff;
+				count = 1;
+			}
+			else if (min_diff == diff)
+			{
+				count++;
+			}
+
+			if (sum < k)
+			{
+				l_p++;
+			}
+			else
+			{
+				r_p--;
+			}
+		}
+		std::cout << count << '\n';
+	}
+}
